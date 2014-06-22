8c4f3019bcb9dd5982862b89ce505b5b29867f6b
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2014-06-22 16:11:16 -0400

    muchos cambios

---
+++ compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ARGUMENT="$1"
+#determine aux name by stripping .tex suffix and adding .aux
+AUXNAME="${ARGUMENT%.tex}.aux"
+pdflatex -shell-escape -interaction=nonstopmode -file-line-error "$ARGUMENT" | grep -i ".*:[0-9]*:.*\|warning" 
+bibtex -terse "$AUXNAME"  
+pdflatex -shell-escape -interaction=nonstopmode -file-line-error "$ARGUMENT" | grep -i ".*:[0-9]*:.*\|warning" 
+#pdflatex thesis_rfuenzalidac
+#bibtex thesis_rfuenzalidac
+#pdflatex thesis_rfuenzalidac
+#pdflatex thesis_rfuenzalidac
